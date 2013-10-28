6acac13c9d20009bb538f51a1901a408d6bc4b39
Chang Hu <ch2950@columbia.edu>
2013-10-27 23:31:13 -0400

    search insert position leetcode

---
+++ Search Insert Position.java
@@ -0,0 +1,34 @@
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+*/
+
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        int low = 0;
+        int high = A.length - 1;
+        
+        while (low <= high) {
+            int mid = (low + high)/2;
+            int cur = A[mid];
+            if (cur == target) {
+                return mid;
+            } else if (cur < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return low;
+    }
+}
