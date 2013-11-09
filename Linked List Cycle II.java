4701e8ca13d913154f8b2695f61bed785f785f7c
Chang Hu <ch2950@columbia.edu>
2013-11-09 00:40:44 -0500

    linked list cycle ii leetcode

---
+++ Linked List Cycle II.java
@@ -0,0 +1,47 @@
+/*
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        ListNode fast = head;
+        ListNode slow = head;
+        boolean loop = false;
+        
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                loop = true;
+                break;
+            }
+        }
+        
+        if (loop) {
+            fast = head;
+            while (fast != slow) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            return fast;
+        } else {
+            return null;
+        }
+    }
+}
