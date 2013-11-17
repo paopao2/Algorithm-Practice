5211a5d1998bf8652b195fa8cf8f2b5245921e25
Chang Hu <ch2950@columbia.edu>
2013-11-17 17:04:14 -0500

    binary tree maximum path sum leetcode

---
+++ Binary Tree Maximum Path Sum.java
@@ -0,0 +1,44 @@
+/*
+Given a binary tree, find the maximum path sum.
+
+The path may start and end at any node in the tree.
+
+For example:
+Given the below binary tree,
+
+       1
+      / \
+     2   3
+Return 6.
+*/
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int maxPathSum(TreeNode root) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        ArrayList<Integer> curMax = new ArrayList<Integer>(1);
+        curMax.add(Integer.MIN_VALUE);
+        subPath(root, curMax);
+        //getMaxSum(root, curMax);
+        return curMax.get(0);
+    }
+    
+    public int subPath(TreeNode root, ArrayList<Integer> curMax) {
+        if (root == null) return 0;
+        int left = Math.max(0, subPath(root.left, curMax));
+        int right = Math.max(0, subPath(root.right, curMax));
+        int max = root.val + left + right;
+        curMax.add(0, Math.max(curMax.get(0), max));
+        //can only return one path (left or right) for recursion use
+        return root.val + Math.max(left, right);
+    }
+}
